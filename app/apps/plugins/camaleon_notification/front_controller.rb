a476d0f9e63afb81f008d48ac164afc44bbe90b7
Owen <owenperedo@gmail.com>
2015-12-02 07:01:24 -0400

    changed into camaleon v2

--- app/apps/plugins/camaleon_notification/front_controller.rb
+++
@@ -1,9 +0,0 @@
-class Plugins::CamaleonNotification::FrontController < Apps::PluginsFrontController
-  include Plugins::CamaleonNotification::MainHelper
-  def index
-    render layout: false
-  end
-
-  # here add your custom functions
-
-end
